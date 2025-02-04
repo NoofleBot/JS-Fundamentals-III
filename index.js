81eaa5e91b212672d439cea965aab5797296758b
NoofleBot <nooflebot@gmail.com>
2025-02-04 11:33:18 -0800

    Added functionality for submit button (broken)

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 console.log('My code is running');
+
+var toDoListItems = [];
+let userInput;
+
+let addToListField = document.querySelector('#taskToAdd');
+addToListField.addEventListener(
+  "input",
+  (eventObject) => userInput = (eventObject.target.value)
+);
+
+ function addToList() {
+console.log(userInput)
+};
+
