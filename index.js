58a960163e5369da5136acdae6b161b75ebbe34a
NoofleBot <nooflebot@gmail.com>
2025-02-04 12:23:56 -0800

    Added tasks now appear on the webpage in a unordered list

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 console.log('My code is running');
 
-var toDoListItems = [];
-let userInput;
-
 var form = document.getElementById('addTaskForm');
 function handleForm(event) { event.preventDefault(); } //prevent page from refreshing when form is submitted
 form.addEventListener('submit', handleForm);
 
 function addToList() {
-console.log(document.getElementById("taskToAdd").value); //extract & console log value of form input on submit
-document.getElementById("taskToAdd").value = "" //make the form text box empty apon submit
+  var inputValue = (document.getElementById('taskToAdd').value); //extract value of form input on submit and put it in inputValue
+  document.getElementById('taskToAdd').value = '' //make the form text box empty apon submit
+
+  let newLi = document.createElement('li');
+  let newLiContent = document.createTextNode(inputValue);
+  newLi.appendChild(newLiContent); //append content of inputValue to newLi
+  let container = document.querySelector("#list");
+  container.appendChild(newLi);
 };
 
